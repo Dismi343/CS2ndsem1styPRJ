971406ffedd6f947aef0bcd0c42584eb8e3b0d87
Nimesh_H_Bandara <121417074+NimeshHB@users.noreply.github.com>
2023-09-29 21:53:26 +0530

    Add files via upload

---
+++ CS2ndsem1styPRJ-1/item.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  font-family: Verdana, sans-serif;
+  margin: 0;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+.row > .column {
+  padding: 0 8px;
+}
+
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+.column {
+  float: left;
+  width: 25%;
+}
+
+/* The Modal (background) */
+.modal {
+  display: none;
+  position: fixed;
+  z-index: 1;
+  padding-top: 100px;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: black;
+}
+
+/* Modal Content */
+.modal-content {
+  position: relative;
+  background-color: #fefefe;
+  margin: auto;
+  padding: 0;
+  width: 90%;
+  max-width: 1200px;
+}
+
+/* The Close Button */
+.close {
+  color: white;
+  position: absolute;
+  top: 10px;
+  right: 25px;
+  font-size: 35px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: #999;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+.mySlides {
+  display: none;
+}
+
+.cursor {
+  cursor: pointer;
+}
+
+/* Next & previous buttons */
+.prev,
+.next {
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  width: auto;
+  padding: 16px;
+  margin-top: -50px;
+  color: white;
+  font-weight: bold;
+  font-size: 20px;
+  transition: 0.6s ease;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+  -webkit-user-select: none;
+}
+
+/* Position the "next button" to the right */
+.next {
+  right: 0;
+  border-radius: 3px 0 0 3px;
+}
+
+/* On hover, add a black background color with a little bit see-through */
+.prev:hover,
+.next:hover {
+  background-color: rgba(0, 0, 0, 0.8);
+}
+
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
+img {
+  margin-bottom: -4px;
+}
+
+.caption-container {
+  text-align: center;
+  background-color: black;
+  padding: 2px 16px;
+  color: white;
+}
+
+.demo {
+  opacity: 0.6;
+}
+
+.active,
+.demo:hover {
+  opacity: 1;
+}
+
+img.hover-shadow {
+  transition: 0.3s;
+}
+
+.hover-shadow:hover {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+
+
+/* Buy button */
+
+
+.button1 {
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+
+.button01 {
+  background-color: white; 
+  color: black; 
+  border: 2px solid #4CAF50;
+}
+
+.button01:hover {
+  background-color: #4CAF50;
+  color: white;
+}
+
+.button02 {
+  background-color: white; 
+  color: black; 
+  border: 2px solid #008CBA;
+}
+
+.button02:hover {
+  background-color: #008CBA;
+  color: white;}
+</style>
+<body>
+
+<h2 style="text-align:center">Select Your Laptop or Destop</h2>
+
+<div class="row">
+  <div class="column">
+       <button class="button1 button01">Buy Now</button>
+       <button class="button1 button02">Add to Cart</button>
+    <img src="https://www.asus.com/media/Odin/Websites/global/Series/9.png" style="width:100%" onclick="openModal();currentSlide(1)" class="hover-shadow cursor">
+  </div>
+  <div class="column">
+       <button class="button1 button01">Buy Now</button>
+       <button class="button1 button02">Add to Cart</button>
+    <img src="https://5.imimg.com/data5/SELLER/Default/2022/7/YT/MQ/YO/42538528/asus-vivobook-15-2022-15-6-39-62-cms-fhd-intel-core-i5-1240p-12th-gen-thin-and-light-laptop.png" style="width:100%" onclick="openModal();currentSlide(2)" class="hover-shadow cursor">
+  </div>
+  <div class="column">
+       <button class="button1 button01">Buy Now</button>
+       <button class="button1 button02">Add to Cart</button>
+    <img src="https://www.asus.com/media/Odin/Websites/global/Series/12.png" style="width:100%" onclick="openModal();currentSlide(3)" class="hover-shadow cursor">
+  </div>
+  <div class="column">
+       <button class="button1 button01">Buy Now</button>
+       <button class="button1 button02">Add to Cart</button>
+    <img src="https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1689155014/Croma%20Assets/Computers%20Peripherals/Laptop/Images/272749_0_m99i3s.png?tr=w-1000" style="width:100%" onclick="openModal();currentSlide(4)" class="hover-shadow cursor">
+  </div>
+</div>
+</br> </br> </br> </br> </br> 
+<div class="row">
+  <div class="column">
+       <button class="button1 button01">Buy Now</button>
+       <button class="button1 button02">Add to Cart</button>
+
+    <img src="https://www.asus.com/media/Odin/Websites/global/Series/9.png" style="width:100%" onclick="openModal();currentSlide(1)" class="hover-shadow cursor">
+  </div>
+  <div class="column">
+       <button class="button1 button01">Buy Now</button>
+       <button class="button1 button02">Add to Cart</button>
+    <img src="https://5.imimg.com/data5/SELLER/Default/2022/7/YT/MQ/YO/42538528/asus-vivobook-15-2022-15-6-39-62-cms-fhd-intel-core-i5-1240p-12th-gen-thin-and-light-laptop.png" style="width:100%" onclick="openModal();currentSlide(2)" class="hover-shadow cursor">
+  </div>
+  <div class="column">
+       <button class="button1 button01">Buy Now</button>
+       <button class="button1 button02">Add to Cart</button>
+    <img src="https://www.asus.com/media/Odin/Websites/global/Series/12.png" style="width:100%" onclick="openModal();currentSlide(3)" class="hover-shadow cursor">
+  </div>
+  <div class="column">
+       <button class="button1 button01">Buy Now</button>
+       <button class="button1 button02">Add to Cart</button>
+    <img src="https://media-ik.croma.com/prod/https://media.croma.com/image/upload/v1689155014/Croma%20Assets/Computers%20Peripherals/Laptop/Images/272749_0_m99i3s.png?tr=w-1000" style="width:100%" onclick="openModal();currentSlide(4)" class="hover-shadow cursor">
+  </div>
+</div>
+
+
+
+
+    
+</body>
+</html>
